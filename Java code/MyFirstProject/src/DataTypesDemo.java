e0cbf9b91bd87271ea06586753426997e610d6df
reemaLaxmikanth <167944510+reemaLaxmikanth@users.noreply.github.com>
2026-07-18 09:29:55 -0400

    Update DataTypes and Data represntation

---
+++ Java code/MyFirstProject/src/DataTypesDemo.java
@@ -0,0 +1,14 @@
+public class DataTypesDemo {
+    public static void demonstrate() {
+        int a = 7;
+        int b = 5;
+        int intSum = a + b;
+
+        float x = 3.5f;
+        float y = 2.2f;
+        float floatSum = x + y;
+
+        System.out.println("Integer addition: " + a + " + " + b + " = " + intSum);
+        System.out.println("Float addition: " + x + " + " + y + " = " + floatSum);
+    }
+}
