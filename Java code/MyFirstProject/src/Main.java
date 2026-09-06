b333374981eeba0ce4d295538d1a6e089fb991bc
reemaLaxmikanth <167944510+reemaLaxmikanth@users.noreply.github.com>
2026-09-06 18:42:17 -0400

    Adding Encasulation, array and string examples

--- Java code/MyFirstProject/src/Main.java
+++ Java code/MyFirstProject/src/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // day 1: Demonstrate data types and their sizes/ranges
-        //DataTypesDemo.demonstrate();
+        // Create the Scanner tool to read inputs
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String customerName = input.nextLine(); // Reads a line of text
+        System.out.println("Customer: " + customerName);
 
-        // day 2: Demonstrate user input, output and operators
-        //App.main(args);
+
+        
+        System.out.print("How many tickets do you want to buy? ");
+        int ticketCount = input.nextInt(); // Reads a whole integer number
+        double singleTicketPrice = 12.50;
+        
+        // Multiply ticket count by the price
+        double totalCost = ticketCount * singleTicketPrice;
+
+
+        
+        System.out.println("\n--- Receipt for " + customerName + " ---");
+        System.out.println("Tickets bought: " + ticketCount);
+        System.out.println("Total Balance due: $" + totalCost);
+        input.close();
     }
 }
+
